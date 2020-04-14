c61ff04a8eafbe9e1db4e8cfb55908aa9ba6ffd9
Bobby White <bobby.white@salesforce.com>
2020-04-14 19:35:37 -0400

    Working, sigh :-)

--- force-app/main/default/lwc/platformEventMonitorTable/platformEventMonitorTable.js
+++ force-app/main/default/lwc/platformEventMonitorTable/platformEventMonitorTable.js
@@ -1,6 +1,43 @@
-import { LightningElement, api, wire } from 'lwc';
-import getInfo from '@salesforce/apex/PlatformEventMonitoringController.getInfo';
+import { LightningElement, wire } from 'lwc';
+import getSubscriberInfo from '@salesforce/apex/PlatformEventMonitoringController.getSubscriberInfo';
 
 export default class PlatformEventMonitorTable extends LightningElement {
-    @wire(getInfo) subscriptionInfos;
+    @wire(getSubscriberInfo) subscriberInfo;
+
+    get errorMessages() {
+        let errors = this.subscriberInfo.error;
+
+        /* Below logic is borrowed from ldsUtils.js in lwc-recipes */
+        if (!Array.isArray(errors)) {
+            errors = [errors];
+        }
+
+        return (
+            errors
+                // Remove null/undefined items
+                .filter(error => !!error)
+                // Extract an error message
+                .map(error => {
+                    // UI API read errors
+                    if (Array.isArray(error.body)) {
+                        return error.body.map(e => e.message);
+                    }
+                    // UI API DML, Apex and network errors
+                    else if (error.body && typeof error.body.message === 'string') {
+                        return error.body.message;
+                    }
+                    // JS errors
+                    else if (typeof error.message === 'string') {
+                        return error.message;
+                    }
+                    // Unknown error shape so try HTTP status text
+                    return error.statusText;
+                })
+                // Flatten
+                .reduce((prev, curr) => prev.concat(curr), [])
+                // Remove empty strings
+                .filter(message => !!message)
+        );
+
+    }
 } 
